8e2b5b0406a696b8155a24dd9d87ff7e10a781a0
AlanPoAlsk <alsk91@gmail.com>
2023-10-25 09:43:09 -0400

    created folders for the files

---
+++ 02_Fundamentals/OOP/Basketball_Dictionaries/basketball_dictionaries.py
@@ -0,0 +1,89 @@
+
+
+class Player:
+    def __init__(self, player):
+        self.name = player['name']
+        self.age = player['age']
+        self.position = player['position']
+        self.team = player['team']
+
+
+
+        
+players = [
+    {
+        "name": "Kevin Durant", 
+        "age":34, 
+        "position": "small forward", 
+        "team": "Brooklyn Nets"
+    },
+    {
+        "name": "Jason Tatum", 
+        "age":24, 
+        "position": "small forward", 
+        "team": "Boston Celtics"
+    },
+    {
+        "name": "Kyrie Irving", 
+        "age":32,
+        "position": "Point Guard", 
+        "team": "Brooklyn Nets"
+    },
+    {
+        "name": "Damian Lillard", 
+        "age":33,
+        "position": "Point Guard", 
+        "team": "Portland Trailblazers"
+    },
+    {
+        "name": "Joel Embiid", 
+        "age":32,
+        "position": "Power Foward", 
+        "team": "Philidelphia 76ers"
+    },
+    {
+        "name": "DeMar DeRozan",
+        "age": 32,
+        "position": "Shooting Guard",
+        "team": "Chicago Bulls"
+    }
+]
+
+
+new_team = []
+for player_ in players:
+    player = Player(player_)
+    new_team.append(player_)
+print(new_team)
+# for player in range(0,len(players)-1):
+#     new_team.append(players[player])
+#     print(f'Player {player+1} : {players[player]}')
+    
+    
+    
+        
+kevin = {
+        "name": "Kevin Durant", 
+        "age":34, 
+        "position": "small forward", 
+        "team": "Brooklyn Nets"
+}
+jason = {
+        "name": "Jason Tatum", 
+        "age":24, 
+        "position": "small forward", 
+        "team": "Boston Celtics"
+}
+kyrie = {
+        "name": "Kyrie Irving", 
+        "age":32,
+        "position": "Point Guard", 
+        "team": "Brooklyn Nets"
+}
+    
+# Create your Player instances here!
+# player_jason = ???
+
+player_jason = Player(jason)
+player_kevin = Player(kevin)
+player_kyrie = Player(kyrie)
